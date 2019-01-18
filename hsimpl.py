ce06a6a3b5ab6d938b11f1db3ea33f264ec38875
Digvijaysingh Gour <ourgour@gmail.com>
2019-01-18 14:20:45 +0530

    Update hsimpl.py

--- hsimpl.py
+++ hsimpl.py
@@ -58,20 +58,38 @@ def sender(node) :
 	node.endpoint.accept()
 
 
-if __name__ == "__main__" :
-
-	n = int(input("Number of participants : "))
-	nodeId = int(input("Enter Node ID : "))
-	nodeHost = input("Enter Node Host : ")
-	nodePort = int(input("Enter Node Port : "))
-	clockwiseNeighbor = int(input("Enter Node Clockwise Neighbor Port : "))
-	antiClockwiseNeighbor = int(input("Enter Node Anti Clockwise Neighbor Port : "))
+def launcher(nodeId,nodeHost,nodePort,clockwiseNeighbor,antiClockwiseNeighbor) :
+	
 	x = Node(nodeId,nodeHost,nodePort,clockwiseNeighbor,antiClockwiseNeighbor)
 	t1 = threading.Thread(target=sender,args=(x,))
 	t1.start()
 	x.joinRing(clockwiseNeighbor,antiClockwiseNeighbor)
 	t1.join()
 
+	
+if __name__ == "__main__" :
+
+	n = int(input("Number of participants : "))
+	threadlist = []
+	for i in range(n):
+		nodeId = int(input("Enter Node ID : "))
+		nodeHost = 'localhost'
+		nodePort = 9000+i
+		if i != n-1 :
+			clockwiseNeighbor = nodePort+1
+		else :
+			clockwiseNeighbor = 9000
+		if i != 0 :
+			antiClockwiseNeighbor = nodePort-1
+		else :
+			antiClockwiseNeighbor = 9000+n-1
+		t = threading.Thread(target=launcher,args=(nodeId,nodeHost,nodePort,clockwiseNeighbor,antiClockwiseNeighbor,))
+		threadlist.append(t)
+		t.start()
+
+
+	for t in threadlist :
+		t.join()
 
 
 
